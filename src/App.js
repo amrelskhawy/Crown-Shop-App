15ab14e054ba4900861d07b5e6b16f51ba242c6e
Amr Elskhawy <amrelskhawy@gmail.com>
2022-07-15 05:17:40 +0200

    first code in the website

---
+++ src/App.js
@@ -0,0 +1,43 @@
+const App = () => {
+    return (
+        <div className="categories-conainer">
+        <div>
+            { /** Img */}
+            <div className="category-body-container">
+                <h2>Hats</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        <div>
+            { /** Img */}
+            <div className="category-body-container">
+                <h2>Jackets</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        <div>
+            { /** Img */}
+            <div className="category-body-container">
+                <h2>Sneakers</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        <div>
+            { /** Img */}
+            <div className="category-body-container">
+                <h2>Women</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        <div>
+            { /** Img */}
+            <div className="category-body-container">
+                <h2>Men</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default App;
